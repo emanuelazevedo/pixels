42eb069360ccb581072d9fadf104748d01e1b312
manu <emanuel.azevedo@ua.pt>
2020-12-11 17:17:01 +0000

    pixels code

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import "../styles/App.scss";
+import Editor from "./Editor"
+
+function App() {
+  return <div className="App">
+    As teached by <a href="https://twitter.com/alekswritescode/status/1335249987823935488">Aleks Popovic</a>
+    <Editor/>
+  </div>
+}
+
+export default App;
